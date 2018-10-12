8ff2dd0cdbc0ad2efcfe0904a9eeeac7996c6b5a
ErrandiI <lempaarkadiusz@gmail.com>
2018-10-12 21:00:04 +0200

    calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+
+        int result2 = calculator.add(3,15);
+        if (result2 == 18)  {
+            System.out.println("test2 OK!");
+        } else {
+            System.out.println("Error2");
+        }
+
+        int result3 = calculator.substract(15,3);
+        if (result3 == 12)  {
+            System.out.println("test3 OK!");
+        } else {
+            System.out.println("Error3");
+        }
+
     }
 }
