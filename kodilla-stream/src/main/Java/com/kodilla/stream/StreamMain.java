36bed8eab37a7b8d901af37ad327a630bac32bca
ErrandiI <lempaarkadiusz@gmail.com>
2018-10-24 21:41:27 +0200

    7.1 final

--- kodilla-stream/src/main/Java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/Java/com/kodilla/stream/StreamMain.java
@@ -7,8 +7,12 @@ import com.kodilla.stream.reference.FunctionalCalculator;
 public class StreamMain {
     public static void main(String[] args) {
         PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("test", (text -> "ABC"));
-        System.out.println("Wyszło?");
+
+        System.out.println(poemBeautifier.beautify("Alfabet tu był", text -> "Tu nie ma alfabetu"));
+        System.out.println(poemBeautifier.beautify("Alfabet tu był", text -> text + ". Tu nie ma alfabetu"));
+        System.out.println(poemBeautifier.beautify("Alfabet tu był", text -> text + "ABC"));
+        System.out.println(poemBeautifier.beautify("Alfabet tu był", text -> text.toUpperCase()));
+        System.out.println(poemBeautifier.beautify("ALFABET TU BYŁ", text -> text.toLowerCase()));
 
     }
 }
