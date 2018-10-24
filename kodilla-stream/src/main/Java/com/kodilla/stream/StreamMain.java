da7b856a457fd2a855e3247122d534e3045f261e
ErrandiI <lempaarkadiusz@gmail.com>
2018-10-24 21:15:29 +0200

    7.1

---
+++ kodilla-stream/src/main/Java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,14 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("test", (text -> "ABC"));
+        System.out.println("Wyszło?");
+
+    }
+}
