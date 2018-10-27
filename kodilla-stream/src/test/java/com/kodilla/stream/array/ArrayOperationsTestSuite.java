351d5408190283f3004bd76ea0bf7260d5aa29d3
ErrandiI <lempaarkadiusz@gmail.com>
2018-10-27 23:48:15 +0200

    7.5 draft

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,21 @@
+package com.kodilla.stream.array;
+
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage() {
+
+    //Given
+    int []table = new int[20];
+
+    for (int i=0; i<20; i++)
+        table[i] = i +1;
+
+
+    //When
+
+    //Then
+
+    }
+}
